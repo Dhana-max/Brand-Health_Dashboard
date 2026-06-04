4b687aa0a1e2c96b71fb9e91ad72ea826ccea811
Dhana-max <dhanalakshmi.r@c5i.ai>
2026-06-04 16:05:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -431,7 +431,7 @@ with tab1:
         filtered_brand_map = {
             k: v for k, v in brand_map.items()
             if any(g.lower() in k.lower() for g in [
-                "LinkedIn", "Indeed", "Google", "TikTok", "Twitter", "Facebook", "YouTube", "Instagram"
+                "LinkedIn", "Indeed", "Google", "TikTok", "Twitter", "Facebook", "YouTube", "Instagram", "Glassdoor", "Snapchat"
         ])
     }
      else:
